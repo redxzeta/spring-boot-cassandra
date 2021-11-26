235e64a3fc6e09a64f552f162baa7a7322d2a501
redxzeta <xzetanzero@gmail.com>
2021-11-26 11:03:47 -0800

    first

---
+++ src/main/java/com/cassandra/cain/CainApplication.java
@@ -0,0 +1,13 @@
+package com.cassandra.cain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cassandra/cain/CainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassandra.cain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
